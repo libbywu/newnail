9b2cf451d2927f006137d7a17f092d83edfe3604
Libby <libbywu12@gmail.com>
2020-08-13 16:08:48 +0800

    202008更新版

---
+++ js/all.js
@@ -0,0 +1,80 @@
+$(document).ready(function () {
+    //nav範例copy
+    (function ($) { // Begin jQuery
+        $(function () { // DOM ready
+            // If a link has a dropdown, add sub menu toggle.
+            $('nav ul li a:not(:only-child)').click(function (e) {
+                $(this).siblings('.nav-dropdown').toggle();
+                // Close one dropdown when selecting another
+                $('.nav-dropdown').not($(this).siblings()).hide();
+                e.stopPropagation();
+            });
+            // Clicking away from dropdown will remove the dropdown class
+            $('html').click(function () {
+                $('.nav-dropdown').hide();
+            });
+            // Toggle open and close nav styles on click
+            $('#nav-toggle').click(function () {
+                $('nav ul').slideToggle();
+            });
+            // Hamburger to X toggle
+            $('#nav-toggle').on('click', function () {
+                this.classList.toggle('active');
+            });
+        }); // end DOM ready
+    })(jQuery); // end jQuery
+
+    //nav end
+    var showSkill = false;
+
+    $('.scrollTop').click(function (e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        var targetPos = $(target).offset().top;
+        $('html, body').animate({ scrollTop: targetPos }, 1000);
+    });
+
+    $(window).scroll(function () {
+        var scrollPos = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        // console.log(scrollPos, windowHeight);
+
+        $('.scrollTop').each(function () {
+            var target = $(this).attr('href');
+            var targetPos = $(target).offset().top;
+            var targetHeight = $(target).outerHeight();
+            if (targetPos -1 <= scrollPos && (targetPos + targetHeight) > scrollPos) {
+                //console.log(target);
+                $('.scrollTop').removeClass('active')
+                $(this).addClass('active');
+            } else {
+                $(this).removeClass('active')
+            }
+        });
+
+        // progress bar
+        var skillTop = $('#skills').position().top;
+        // console.log('skillTop', skillTop);
+        if (skillTop <= (scrollPos + windowHeight / 2) && !showSkill) {
+            showSkill = true;
+            $('#skills .progress-bar').each(function () {
+                var thisValue = $(this).data('progress');
+               console.log('thisValue', thisValue);
+                $(this).css('width', thisValue + '%');
+            });
+        }
+
+        // animated
+        $('.animated').each(function () {
+            var thisPos = $(this).offset().top;
+            if ((windowHeight + scrollPos) >= thisPos) {
+                $(this).addClass('fadeIn');
+            }
+        });
+
+        // bg scroll 
+        $('#profiles').css('background-position-y', -(scrollPos / 2) + 'px')
+        $('#header-ele').css('transform', 'translateY( ' + (scrollPos / 2) + 'px )')
+    });
+
+});
